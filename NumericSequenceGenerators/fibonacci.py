517fa98e229e45704eb8224371f5dd3ccb3f9a71
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-25 22:30:14 -0700

    fix bug in fibonacci generator

--- NumericSequenceGenerators/fibonacci.py
+++ NumericSequenceGenerators/fibonacci.py
@@ -1,10 +1,12 @@
 # Author: M.A.R.C Original Music
 # ©2023
 
+# f(0) = 1, f(0) = 1, f(n) = f(n-1) + f(n-1)
+
 def generate_sequence(num_of_terms = 10) -> list[int]:
     sequence = [1,1]
 
-    for i in range(2, num_of_terms):
-        sequence.append(sequence[i-2] + sequence[-1])
+    for i in range(len(sequence), num_of_terms):
+        sequence.append(sequence[i-1] + sequence[i-2])
 
     return sequence
