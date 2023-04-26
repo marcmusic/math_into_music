fab339458c351bde9b2e99ea9cfc90a65c220625
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-25 23:07:43 -0700

    Add prime number generator

--- NumericSequenceGenerators/_test.py
+++ NumericSequenceGenerators/_test.py
@@ -1,7 +1,9 @@
 import fibonacci
 import pascal_triangle
 import perrin
+import prime
 
 # print(pascal_triangle.generate_sequence())
-print(fibonacci.generate_sequence())
-print(perrin.generate_sequence(10))
+# print(fibonacci.generate_sequence())
+# print(perrin.generate_sequence(10))
+print(prime.generate_sequence(100))
---
+++ NumericSequenceGenerators/prime.py
@@ -0,0 +1,21 @@
+# Author: M.A.R.C Original Music
+# ©2023
+
+sequence = [2]
+
+def generate_sequence(num_of_terms = 10) -> list[int]:
+    next_natural_number = sequence[-1] + 1
+
+    for i in range(len(sequence), num_of_terms):
+        while not is_prime(next_natural_number):
+            next_natural_number += 1
+        sequence.append(next_natural_number)
+        next_natural_number += 1
+
+    return sequence
+
+def is_prime(num) -> bool:
+    for i in range (2, num // 2 + 1):
+        if num % i == 0:
+            return False
+    return True
