88fe70dd24197be4b0ff2a02036bf4877006be37
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-28 16:38:59 -0700

    Update test file

--- NumericSequenceGenerators/_test.py
+++ NumericSequenceGenerators/_test.py
@@ -4,10 +4,11 @@ import perrin
 import prime_numbers
 import happy_numbers
 import lucky_numbers
+import exponential_numbers
 
 # print(pascal_triangle.generate_sequence())
 # print(fibonacci.generate_sequence())
 # print(perrin.generate_sequence(10))
 # print(prime.generate_sequence(100))
 # print(happy_numbers.generate_sequence(10))
-print(lucky_numbers.generate_sequence(100))
+print(exponential_numbers.generate_sequence(100, 3))
