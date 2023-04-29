ab0797f0a999db6197aa155d9cbb2892a08b7f7b
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-29 14:19:26 -0700

    Update Exponential Generator title and description

--- numeric_sequence_generators/exponential_numbers.py
+++ numeric_sequence_generators/exponential_numbers.py
@@ -2,11 +2,11 @@
 # ©2023
 # Exponential Numbers Sequence Generator
 
-# f(n) = n^2
+# f(n) = n^^2
 
 from .util.numeric_sequence import NumericSequence
 
 def generate_sequence(num_of_terms = 10, exponent = 2) -> NumericSequence:
     sequence =  [pow(i+1, exponent) for i in range(num_of_terms)]
-    return NumericSequence(f'Exponential-{exponent}',f'Exponential Numbers n^{exponent}', sequence, f'f(n) = n^{exponent}')
+    return NumericSequence(f'Exponential-{exponent}',f'Exponential Numbers n^^{exponent}', sequence, f'f(n) = n^^{exponent}')
 
