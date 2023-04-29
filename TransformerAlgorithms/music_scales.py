4f0853517b6c4c23f713c34cc9995da1530f23bf
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-28 17:04:16 -0700

    Add Music Scales File

---
+++ TransformerAlgorithms/music_scales.py
@@ -0,0 +1,7 @@
+# Author: M.A.R.C Original Music
+
+PENTATONIC_SCALE = ["A", "C", "D", "F", "G"]
+DIATONIC_SCALE = ["B", "C", "D", "E", "F", "G", "A"]
+CHROMATIC_SCALE = ["B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#"]
+
+MUSIC_SCALES = [("Pentatonic Scale", PENTATONIC_SCALE), ("Diatonic Scale", DIATONIC_SCALE), ("Chromatic Scale", CHROMATIC_SCALE)]
