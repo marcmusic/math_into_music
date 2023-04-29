58d8ea9da2d1d232f68c1496fbfc596d3e437b63
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-28 23:02:15 -0700

    Add inverse musical scales

--- TransformerAlgorithms/music_scales.py
+++ TransformerAlgorithms/music_scales.py
@@ -4,4 +4,15 @@ PENTATONIC_SCALE = ["A", "C", "D", "F", "G"]
 DIATONIC_SCALE = ["B", "C", "D", "E", "F", "G", "A"]
 CHROMATIC_SCALE = ["B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#"]
 
-MUSIC_SCALES = [("Pentatonic Scale", PENTATONIC_SCALE), ("Diatonic Scale", DIATONIC_SCALE), ("Chromatic Scale", CHROMATIC_SCALE)]
+INVERSE_PENTATONIC_SCALE = ["D", "C", "A", "G", "F"]
+INVERSE_DIATONIC_SCALE = ["D", "C", "B", "A", "G", "F", "E"]
+INVERSE_CHROMATIC_SCALE = ["C#", "C", "B", "A#", "A", "G#", "G", "F#", "F", "E", "D#", "D"]
+
+MUSIC_SCALES = {
+    "Pentatonic Scale": PENTATONIC_SCALE,
+    "Inverse Pentatonic Scale": INVERSE_PENTATONIC_SCALE,
+    "Diatonic Scale": DIATONIC_SCALE,
+    "Inverse Diatonic Scale": INVERSE_DIATONIC_SCALE,
+    "Chromatic Scale": CHROMATIC_SCALE,
+    "Inverse Chromatic Scale": INVERSE_CHROMATIC_SCALE
+}
