7089f6e01dbbba5220cdb120584d850025432108
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-04-28 23:50:49 -0700

    Add description field to NumericSequence.

--- NumericSequenceGenerators/util/numeric_sequence.py
+++ NumericSequenceGenerators/util/numeric_sequence.py
@@ -1,8 +1,9 @@
 class NumericSequence:
-    def __init__(self, id, name, numeric_sequence) -> None:
+    def __init__(self, id, name, numeric_sequence, description) -> None:
         self.id = id
         self.name = name
         self.numeric_sequence = numeric_sequence
+        self.description = description
 
     def __str__(self) -> str:
-        return str(self.id) + "\n" + str(self.name) + "\n" + str(self.numeric_sequence)
+        return str(self.id) + "\n" + str(self.name) + "\n" + str(self.description) + "\n" + str(self.numeric_sequence)
