a6f5fd0a7311e48924d6b9a15f2e453e89837a37
M.A.R.C Original Music <131841515+marcmusic@users.noreply.github.com>
2023-10-31 01:01:27 -0700

    Add Whole Tone Scale to music_scales.py

--- transformer_algorithms/util/music_scales.py
+++ transformer_algorithms/util/music_scales.py
@@ -4,12 +4,9 @@ from .diatonic_mode import DiatonicMode
 
 
 PENTATONIC_SCALE = ['C', 'D', 'F', 'G', 'A']
+WHOLE_TONE_SCALE = ['C', 'D', 'E', 'F#', 'G#', 'A#']
 DIATONIC_SCALE = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 CHROMATIC_SCALE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-
-# INVERSE_PENTATONIC_SCALE = ["D", "C", "A", "G", "F"]
-# INVERSE_DIATONIC_SCALE = ["D", "C", "B", "A", "G", "F", "E"]
-# INVERSE_CHROMATIC_SCALE = ["C#", "C", "B", "A#", "A", "G#", "G", "F#", "F", "E", "D#", "D"]
 
 def offset_scale(music_scale, offset) -> list[int]:
     return music_scale[offset:] + music_scale[0:offset]
@@ -21,6 +18,8 @@ def invert_scale(music_scale) -> list[int]:
 MUSIC_SCALES = {
     "C-Pen": PENTATONIC_SCALE,
     "C-Ipen": invert_scale(PENTATONIC_SCALE[:]),
+    "C-Wts": WHOLE_TONE_SCALE,
+    "C-Iwts": invert_scale(WHOLE_TONE_SCALE[:]),
     "C-Dia": DIATONIC_SCALE,
     "C-Idia": invert_scale(DIATONIC_SCALE[:]),
     "C-Chr": CHROMATIC_SCALE,
